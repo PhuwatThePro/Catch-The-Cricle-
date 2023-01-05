0533c469733c166aab77c48409ba0ed5d9133f04
PhuwatThePro <121108583+PhuwatThePro@users.noreply.github.com>
2023-01-05 20:11:45 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const circle = document.getElementById("circle");
+const container = document.getElementById("container");
 
-circle.addEventListener("click", function() {
+container.addEventListener("click", function(event) {
   const x = Math.floor(Math.random() * window.innerWidth);
   const y = Math.floor(Math.random() * window.innerHeight);
+  const circle = document.createElement("div");
+  circle.classList.add("circle");
   circle.style.left = x + "px";
   circle.style.top = y + "px";
+  container.appendChild(circle);
 });
+
